f8f20bda6888b21963a9e349958c098dcb47e83d
zhangshy <zaozh1990@163.com>
2014-07-26 10:31:01 +0000

    添加命令行参数

--- src/myNet/getWeb.py
+++ src/myNet/getWeb.py
@@ -8,8 +8,8 @@ import urllib.request
 import time
 import os
 from html.parser import HTMLParser
+import argparse
 
-index = 0
 filePath = "/home/zsy/lqbz/pictures/"
 
 '''
@@ -22,14 +22,13 @@ class MyHTMLParser(HTMLParser):
         if tag=="img":            
             if attrs[0][0]=="src" or attrs[0][0]=="r_src":
                 url = attrs[0][1]
-                print(url)
                 if (url.startswith("http")):
                     if url[-4:-3]=='.':
                         fileNameindex = url.rfind("/")
                         fileName = url[fileNameindex:]
                         fileName = filePath + fileName
                         urllib.request.urlretrieve(url, fileName)
-                        index += 1
+                        print(".", end=" ")
                 
 
 ##根据charset=选项确定编码方式
@@ -51,42 +50,55 @@ def getEncodeType(context):
                                     #GBK 是 GB2312的扩展 ,除了兼容GB2312外，它还能显示繁体中文，还有日文的假名
     return decodeType
 
-     
-url = "http://news.baidu.com/"
-#url = "http://www.youku.com/"
-headers = {'User-agent':'Mozilla/5.0',
-           'cache-control':'no-cache',
-           'accept':'*/*'
-           }
-req = urllib.request.Request(url)
-for k, v in headers.items():
-    #print(k, v)
-    req.add_header(k, v)
-r = urllib.request.urlopen(req)
-context = r.read()
-decodeType=getEncodeType(context)
-data = context.decode(decodeType)   #使用gbk还是utf-8看具体内容。youku使用decode("UTF-8")
-#print(data)
-global filePath
-localDir = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()) #获取当前时间
-filePath = filePath + localDir;
-if not os.access(filePath, os.R_OK):
-    print("目录不存在")
-    os.mkdir(filePath, mode=0o777)
-
-parser = MyHTMLParser()
-parser.feed(data)
-
 '''
-url="http://d.hiphotos.baidu.com/news/pic/item/c9fcc3cec3fdfc03076fe377d63f8794a5c226ed.jpg"
-filePath = "/home/zsy/lqbz/pictures/"
-localDir = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()) #获取当前时间
-filePath = filePath + localDir;
-if not os.access(filePath, os.R_OK):
-    print("目录不存在")
-    os.mkdir(filePath, mode=0o777)
-
-print(localDir)
-fileName = filePath + "/" + "1.jpg"
-urllib.request.urlretrieve(url, fileName);  #将url内容存入文件
+创建文件保存路径
 '''
+def creatOutPath():
+    global filePath
+    localDir = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()) #获取当前时间
+    filePath = filePath + localDir;
+    if not os.access(filePath, os.R_OK):
+        print("目录不存在")
+        os.mkdir(filePath, mode=0o777)        
+'''
+获取url内容
+'''
+def getUrlContext(url):
+    headers = {'User-agent':'Mozilla/5.0',
+               'cache-control':'no-cache',
+               'accept':'*/*'
+               }
+    req = urllib.request.Request(url)
+    for k, v in headers.items():
+        #print(k, v)
+        req.add_header(k, v)
+    r = urllib.request.urlopen(req)
+    context = r.read()
+    decodeType=getEncodeType(context)
+    data = context.decode(decodeType)   #使用gbk还是utf-8看具体内容。youku使用decode("UTF-8")
+    return data
+    
+if __name__=="__main__":  
+    url = "http://news.baidu.com/"
+    #url = "http://www.youku.com/"
+    global filePath
+    ##命令行参数解析
+    parser = argparse.ArgumentParser(description="处理网页内容")
+    parser.add_argument('-u', '--url', dest="url", help="要访问的网页url")
+    parser.add_argument('-o', '--output', dest="filePath", help="文件要保存的路径")
+    parser.add_argument('--image', action='store_true', help="使用保存图片操作")
+    argRet = parser.parse_args()
+    print(argRet)
+    #使用命令行中的参数
+    url = argRet.url if argRet.url!=None else url   
+    filePath = argRet.filePath if argRet.filePath!=None else filePath
+    print(url, filePath)
+    
+    if argRet.image:
+        data = getUrlContext(url)
+        creatOutPath()
+        parser = MyHTMLParser()
+        parser.feed(data)
+        
+    print("fun end")
+    
